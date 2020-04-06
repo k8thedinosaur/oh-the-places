35bc7682263987a4799831983885824bed8c1db8
Kate Farkas <kate.m.farkas@gmail.com>
2020-04-06 11:42:18 -0700

    add more place examples, fail at putting individual 'Place' instances in 'Places' object

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,11 @@
-// function Places() {
-//   this.places = []
-//   this.currrentID = 0;
-// }
+function Places() {
+  this.places = []
+  this.currrentID = 0;
+}
+
+Places.prototype.addPlace = function(place) {
+  this.Places.push(place);
+}
 
 function Place(location, landmarks, timeOfYear, notes) {
   this.location = location;
@@ -10,6 +14,13 @@ function Place(location, landmarks, timeOfYear, notes) {
   this.notes = notes;
 }
 
-var utah = new Place("Utah", ["Red Rocks", "Hurricane"], "April 2018", "Saw some weirdos bury a barrel in the desert; thought it had a dead body in it; called the cops; turns out it was just full of water." )
+var southernUtah = new Place("Southern Utah", ["Red Rocks", "Hurricane"], "April 2018", "Saw some weirdos bury a barrel in the desert; thought it had a dead body in it; called the cops; turns out it was just full of water." )
 
-console.log(utah);
+var oregonCoast = new Place("Oregon Coast", ["Seaside", "Astoria"], "July 2019", "Went with Roman and Momo for 4th of July.")
+
+var southernColorado = new Place("Southern Colorado", ["Cortez", "Dolores"], "October 2019", "Worked as a cook for a hunting camp. Hung out with horses. Made bank.")
+
+
+
+// console.log(southernUtah, oregonCoast, southernColorado);
+console.log(Places);
